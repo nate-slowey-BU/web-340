070f7ea26caa4d5a763c2826b122d22ca59c4fab
nate-slowey-BU <nslowey@my365.bellevue.edu>
2024-10-26 16:09:47 -0500

    week 1 weight-converter complete

--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,10 +1,25 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author: Nate Slowey
+ * Date: 10/26/24
+ * File Name: weight-converter.js
+ * Description: Converts weight in pounds to kilograms
+ */
 
 "use strict";
 
-// TODO: Implement the weight conversion logic here
+function main() {
+  if (process.argv.length != 3) {
+    console.error("stderr: Usage: node weight-converter.js <pounds>");
+    process.exit(1);
+  }
+  const pounds = process.argv[2];
+  if (isNaN(pounds)) {
+    console.error("stderr: input must be a number");
+    process.exit(1);
+  }
+
+  const kilograms = pounds * 0.453592;
+  console.log(`${kilograms.toFixed(2)}`);
+}
+
+main();
